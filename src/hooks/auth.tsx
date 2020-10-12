34576f64eb16584d515e211822cbfe70d29ebeb9
Matheus Antonio <matheusantoniof@gmail.com>
2020-10-12 12:04:35 -0300

    feat: show routes if user loged ou unloged

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -20,6 +20,7 @@ interface SignInCredentials {
 
 interface AuthContextData {
   user: Record<string, unknown>;
+  loading: Boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -28,7 +29,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     async function loadStoragedData(): Promise<void> {
       const [token, user] = await AsyncStorage.multiGet([
@@ -38,6 +39,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
       if (token[1] && user[1])
         setData({ token: token[1], user: JSON.parse(user[1]) });
+
+      setLoading(false);
     }
     loadStoragedData();
   }, []);
@@ -65,7 +68,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
